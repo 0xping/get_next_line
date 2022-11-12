cc9114dc7ac7c97ddedf1c00dcf8fde88cf1b7da
Ahmed Ait Lfdil <aait-lfd@e1r12p2.1337.ma>
2022-11-12 18:33:52 +0100

    add BONUS

---
+++ get_next_line_bonus.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 17:13:27 by aait-lfd          #+#    #+#             */
+/*   Updated: 2022/11/12 18:32:55 by aait-lfd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+typedef struct s_data
+{
+	char	**stc;
+	char	**result;
+	char	**buff;
+}			t_data;
+
+char	*get_line(t_data *data, int fd, ssize_t rd)
+{
+	char	*tmp;
+
+	while (rd >= 0)
+	{
+		(*(data->buff))[rd] = 0;
+		tmp = *(data->stc);
+		*(data->stc) = ft_strjoin(*(data->stc), *(data->buff));
+		ft_free(&tmp, 0);
+		if (ln_index(*(data->stc)) >= 0)
+		{
+			tmp = *(data->stc);
+			split_with_nl(data->result, data->stc, *(data->stc));
+			ft_free(&tmp, data->buff);
+			return (*(data->result));
+		}
+		if (rd == 0)
+		{
+			(**data->stc) && (*(data->result) = ft_strdup(*(data->stc)));
+			ft_free(data->buff, data->stc);
+			return (*(data->result));
+		}
+		rd = read(fd, *(data->buff), BUFFER_SIZE);
+	}
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stc[9999999];
+	char		*buff;
+	int			rd;
+	char		*result;
+	t_data		data;
+
+	result = 0;
+	buff = malloc(BUFFER_SIZE + 1);
+	if (buff)
+	{
+		rd = read(fd, buff, BUFFER_SIZE);
+		if (rd < 0)
+		{
+			ft_free(&stc[fd], &buff);
+			return (0);
+		}
+		data.stc = &stc[fd];
+		data.buff = &buff;
+		data.result = &result;
+		return (get_line(&data, fd, rd));
+	}
+	return (buff);
+}
+
+int	ln_index(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (s)
+	{
+		while (s[i])
+		{
+			if (s[i] == '\n')
+				return (i);
+			i++;
+		}
+	}
+	return (-1);
+}
+
+void	*ft_free(char **s1, char **s2)
+{
+	if (s1 && *s1)
+	{
+		free(*s1);
+		*s1 = 0;
+	}
+	if (s2 && *s2)
+	{
+		free(*s2);
+		*s2 = 0;
+	}
+	return (0);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aait-lfd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 17:13:34 by aait-lfd          #+#    #+#             */
+/*   Updated: 2022/11/12 18:29:39 by aait-lfd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*p;
+	int		i;
+
+	p = malloc(ft_strlen(s1) + 1);
+	i = 0;
+	if (p)
+	{
+		while (s1[i])
+		{
+			p[i] = s1[i];
+			i++;
+		}
+		p[i] = 0;
+	}
+	return (p);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*result;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 && !s2)
+		return (0);
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	if (s1 && !s2)
+		return (ft_strdup(s1));
+	result = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (result)
+	{
+		i = 0;
+		while (s1[i])
+		{
+			result[i] = s1[i];
+			i++;
+		}
+		j = 0;
+		while (s2[j])
+			result[i++] = s2[j++];
+		result[i] = 0;
+	}
+	return (result);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*subs;
+	size_t	i;
+	size_t	slen;
+
+	if (!s)
+		return (0);
+	slen = ft_strlen(s);
+	if (start >= slen)
+		return (ft_strdup(""));
+	if (slen - start < len)
+		len = slen - start;
+	subs = malloc(len + 1);
+	if (subs)
+	{
+		i = 0;
+		while (i < len && s[start + i])
+		{
+			subs[i] = s[start + i];
+			i++;
+		}
+		subs[len] = 0;
+	}
+	return (subs);
+}
+
+void	split_with_nl(char **s1, char **s2, char *s3)
+{
+	*s1 = ft_substr(s3, 0, ln_index(s3) + 1);
+	*s2 = ft_strdup(s3 + ln_index(s3) + 1);
+}
